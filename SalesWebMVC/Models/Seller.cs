da87b21d8b98b93e6dd3eb1dc5577d043486af20
ErickBatistaa <erick.santos067@gmail.com>
2020-02-09 13:45:40 -0300

    App locale, number and date formatting

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMVC.Models
@@ -8,8 +9,17 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)] //inserindo um link em cada email para já direcionar algum email usando algum app de email que deseja
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")] //customizando o titulo da data de nascimento do vendedor
+        [DataType(DataType.Date)] //retirando o campo de horário e deixando apenas o campo de data no formulário
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")] //inserindo o formato da data desse campo
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")] //inclui 2 casas decimais depois da virgula para o numero informado
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
